b432b7c2f046f63a38b27f375a594def1d2bc842
g19925mm <75403433+g19925mm@users.noreply.github.com>
2021-01-08 23:10:20 +0900

    Update main1.js

--- main1.js
+++ main1.js
@@ -7,8 +7,8 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+let storyText = '気温は摂氏-1度で外は寒かった。:insertx:は出かけた。:inserty:に着いたら,:insertz:。体重50kgのBobは驚いたが、:insertx:は驚かなかった。:inserty:ではよくあることだった';
+let insertX = ['ねこ','いぬ','くま'];
 let insertY = ['the soup kitchen','Disneyland','the White House'];
 let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
@@ -31,7 +31,7 @@ function result() {
     newStory = newStory.replace('Bob',name);
   }
 
-  if(document.getElementById("uk").checked) {
+  if(document.getElementById("us").checked) {
     const weight = Math.round(300*0.0714286) + ' stone';
     const temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
     newStory = newStory.replace('94 fahrenheit',temperature);
